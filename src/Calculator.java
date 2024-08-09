c3a8b6b43af37dd22f8b8e06fb278bddced153bf
juanjhiguita <juan.higuita@globant.com>
2024-08-08 20:48:05 -0500

    sum and sub methods

--- src/Calculator.java
+++ src/Calculator.java
@@ -1,5 +1,11 @@
 package src;
 
 public class Calculator {
+    public double sum (double num1, double num2){
+        return num1 + num2;
+    }
 
+    public double sub (double num1, double num2){
+        return num1 - num2;
+    }
 }
