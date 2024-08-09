57606f920e7b7314dc578cc5e3f9c5161b7dd1b8
juanjhiguita <juan.higuita@globant.com>
2024-08-08 20:54:11 -0500

    multiply and divide methods

--- src/Calculator.java
+++ src/Calculator.java
@@ -1,5 +1,11 @@
 package src;
 
 public class Calculator {
+    public double multiply(double num1, double num2){
+        return num1 * num2;
+    }
 
+    public double divide (double num1, double num2){
+        return num1 / num2;
+    }
 }
